23912872304e0fd19f553cb8a42034ac97a5246b
AaronMullan <aaroncmullan@gmail.com>
2019-12-20 13:50:12 -0800

    writing record schema

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const { Router } =  require('express');
+const User = require('../lib/models/User');
+
+module.exports = Router()
+  .post('/signup', (req, res, next) => {
+    User
+      .create(req.body)
+      .then(user => res.send(user))
+      .catch(next);
+  })
+
+  .post('/login', (req, res, next) => {
+    User
+      .authenticate(req.body)
+      .then(user => res.send(user))
+      .catch(next);
+  });
+
